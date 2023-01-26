09100f8045097ea4a06d50e8b8f5d6cbd6bd524e
do Nascimento <mott@gft.com>
2023-01-26 18:49:13 -0300

    Alterações no CRUD de Alunos , Layout CSS

--- src/app/components/views/config/config.component.ts
+++ src/app/components/views/config/config.component.ts
@@ -20,11 +20,4 @@ export class ConfigComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  tiles: Tile[] = [
-    {text: 'One', cols: 1, rows: 2, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 2, color: 'lightpink'},
-    {text: 'Four', cols: 1, rows: 2, color: '#DDBDF1'},
-  ];
-
 }
